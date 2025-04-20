78f929526769a3edbbbd206a694ccf8f448111aa
zyx <3051857358@qq.com>
2025-04-21 00:02:41 +0800

    员工分页查询代码

--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,16 +1,20 @@
 package com.sky.service.impl;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import com.sky.constant.MessageConstant;
 import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
 import com.sky.context.BaseContext;
 import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import com.sky.exception.AccountLockedException;
 import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
+import com.sky.result.PageResult;
 import com.sky.service.EmployeeService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +22,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
@@ -87,6 +92,22 @@ public class EmployeeServiceImpl implements EmployeeService {
         employee.setUpdateUser(BaseContext.getCurrentId());
 
         employeeMapper.insert(employee);
+    }
+
+
+    @Override
+    public PageResult pageQuery(EmployeePageQueryDTO employeePageQueryDTO){
+        //select * from employee limit 0,10 mysql语句
+        //开始分页查询,运用PageHelper,其使用mybatis拼接
+        //下面这段代码会设置一个LocalPage创建一个本地的Page,Page源码继承的是ThreadLocal,
+        // 因此调用pageHelper就将page存到线程存储空间去了，分页查询之前把page的页码和每页记录数传过去计算sql语句的limit
+        PageHelper.startPage(employeePageQueryDTO.getPage(),employeePageQueryDTO.getPageSize());
+
+        Page<Employee> page =  employeeMapper.pageQuery(employeePageQueryDTO);
+
+        long total = page.getTotal(); //得到pageResult的总记录数
+        List<Employee> records = page.getResult();
+        return new PageResult(total,records);
     }
 
 }
